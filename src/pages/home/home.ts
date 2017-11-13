8371f05716f813ea7cf58ff08067faf2d92cbc7e
josens <zeneto1@gmail.com>
2017-11-13 01:16:28 -0200

    teste

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HospedagemProvider } from '../../providers/hospedagem/hospedagem';
+
+declare var google:any;
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,71 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef: ElementRef;
+  
+  public hospedagens: any [];
+  map: any;
 
+  constructor(public navCtrl: NavController,
+              public hosProvider: HospedagemProvider,) {
+
+  }
+
+  public getHospAll() {
+    
+    this.hosProvider.getAllHospedagem()
+    .subscribe(
+      (hospedagens) => {
+        this.hospedagens = hospedagens.lista;
+        this.getMarkers(this.hospedagens);  
+      },
+      (erros) => {
+        console.log('Error', erros);
+      }
+    )  
+  }
+
+  public displayMap() {
+    let latLng = new google.maps.LatLng(-3.7317914, -38.5114384);
+    
+        let mapOptions = {
+          center: latLng,
+          zoom: 10,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        this.map = new google.maps.Map(this.mapRef.nativeElement, mapOptions);
+  }
+
+  public getMarkers(hospedagens) {
+    console.log(hospedagens.length);
+    for (let i = 0; i < hospedagens.length; i++) {
+       this.addMarkers(hospedagens[i]);
+    }
+
+    
+  }
+
+  public addMarkers(hospedagem) {
+    var infowindow = new google.maps.InfoWindow();
+    var position = new google.maps.LatLng(hospedagem.latitude, hospedagem.longitude);
+    var HospedagemMarker = new google.maps.Marker({position: position, title: hospedagem.nome});
+    google.maps.event.addListener(HospedagemMarker, 'click', (function(HospedagemMarker) {
+      return function() {
+        infowindow.setContent(hospedagem.nome);
+        console.log(hospedagem);
+        infowindow.open(this.map, HospedagemMarker);
+      }
+    })(HospedagemMarker));
+    console.log(hospedagem.nome);
+    HospedagemMarker.setMap(this.map);
+
+}
+
+  ionViewDidLoad() {
+    this.displayMap();
+    this.getHospAll();
+    console.log(this.mapRef);
+    console.log('ionViewDidLoad Hospedagem');
   }
 
 }
